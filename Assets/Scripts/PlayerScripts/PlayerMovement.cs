603d2c1fe89eb8b4d733de396cac1412f6f08739
carlosrodcor <crod143@lsu.edu>
2025-03-20 18:25:50 -0500

    started design for the commons and capstone gallery

--- Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -1,92 +1,49 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
+
 public class PlayerMovement : MonoBehaviour
 {
+ public Rigidbody2D rb;
+    public Animator anim;
+    public int facingDirection = 1;
+    public int speed = 5;
 
 
-    [Header("Movement")]
-    [SerializeField] private float moveSpeed = .5f ;
-    private Vector2 movementDirection;
-    private Vector2 currentInput;
-
-    [Header("Animations")]
-    [SerializeField] private Animator anim;
-
-    private string lastDirection = "Down";
-
-    private Rigidbody2D rb;
-
-    private void Awake()
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+       
+        
     }
 
-
-
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        HandleAnimations();
-    }
-    private void FixedUpdate()
-    {
-        rb.linearVelocity = movementDirection * moveSpeed;
-    }
+     
 
-    private void HandleAnimations()
-    {
-        if (anim == null)
-        {
-            return;
-        }
-        string animationName = "";
-        if (movementDirection == Vector2.zero)
-        {
-            animationName = "Idle";
-        }
-        else
-        {
-            animationName = "Walking";
-        }
-
-        anim.Play(animationName + lastDirection);
-    }
-
-    private Vector3 GetDirection(Vector3 input)
-    {
-        Vector3 finalDirection = Vector3.zero;
-        if (input.y > 0.01f)
-        {
-            lastDirection = "Up";
-            finalDirection = new Vector2(0, 1);
-        }
-        else if (input.y < -0.01f)
-        {
-            lastDirection = "Down";
-            finalDirection = new Vector2(0, -1);
-        }
-        else if (input.x > 0.01f)
-        {
-            lastDirection = "Right";
-            finalDirection = new Vector2(1, 0);
-        }
-        else if (input.x < -0.01f)
-        {
-            lastDirection = "Left";
-            finalDirection = new Vector2(-1, 0);
-        }
-        else 
-        {
-            finalDirection = Vector2.zero;
-        }
-        return finalDirection;
-    }
-
-    private void OnMove(InputValue value)
-    {
-        currentInput = value.Get<Vector2>().normalized;
-        movementDirection = GetDirection(currentInput);
-    }
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
     
+        if (horizontal > 0 && transform.localScale.x < 0 || horizontal < 0 && transform.localScale.x > 0)
+        {               
+            flip();
+        }
+        rb.linearVelocity = new Vector2(horizontal, vertical) * speed;
+
+        anim.SetFloat("horizontal", Mathf.Abs(horizontal));
+        anim.SetFloat("vertical", Mathf.Abs(vertical));
+    }
+
+       void flip()
+    {
+        facingDirection *= -1;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+    }
+
+    
+
+    
+   // in case we wish to implement 8 directional movement and animations i could set checks in fixed update to swithc the animation based
+   // on the input. or i could probably do it through the animator. in a simpler way. 
+
+   // this code still deoesn't control the change of direction from up to down.
 
 }
